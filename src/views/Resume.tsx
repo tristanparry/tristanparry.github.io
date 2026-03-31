8ba4fa3630eab8b271f6b498f9f07a4cfc27ceee
Tristan Parry <tristanparry@gmail.com>
2026-03-31 13:00:24 -0400

    Fix resume page viewport height

--- src/views/Resume.tsx
+++ src/views/Resume.tsx
@@ -54,7 +54,7 @@ const Resume = () => {
   };
 
   return (
-    <main className="h-screen-safe flex flex-1 flex-col justify-between">
+    <main className="min-h-screen-safe flex flex-1 flex-col justify-between">
       <div
         className={clsx(
           'sticky top-0 z-20 flex items-center justify-between px-4 py-2 select-none',
