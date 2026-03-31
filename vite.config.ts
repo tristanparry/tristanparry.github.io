21e4b3feb2694a4046c8b4d3cfbc0c424b3d6d49
Tristan Parry <tristanparry@gmail.com>
2026-03-30 22:47:32 -0400

    Vite deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr(), tailwindcss()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
